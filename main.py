03ae28956f7c35ded00de0c066e01c9b95c2b711
LOVELY <kotagindeman@gmail.com>
2025-03-31 17:46:35 +0300

    feat: method draw backround

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ pg.init()
 SCREEN_WIDTH = 900
 SCREEN_HEIGHT = 550
 
+ICON_SIZE = 80
+PADDING = 5
+
+
+
+
+def load_image(file, width, height):
+    image = pg.image.load(file).convert_alpha()
+    image = pg.transform.scale(image, (width, height))
+    return image
+
 
 class Game:
     def __init__(self):
@@ -15,6 +26,14 @@ class Game:
         self.screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pg.display.set_caption("Виртуальный питомец")
 
+
+        self.happiness = 100
+        self.satiety = 100
+        self.health = 100
+
+
+        self.background = load_image('image/background.png', SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.happiness = load_image('image/happiness.png', ICON_SIZE, ICON_SIZE)
         self.run()
 
     def run(self):
@@ -33,7 +52,13 @@ class Game:
         ...
 
     def draw(self):
+
+        self.screen.blit(self.background, (0, 0))
+        self.screen.blit(self.happiness, (PADDING, PADDING))
+
         pg.display.flip()
+
+
 
 
 if __name__ == "__main__":
