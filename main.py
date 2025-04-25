6b815d7881482b42d1cab2c8997576fc6ea848f0
LOVELY <kotagindeman@gmail.com>
2025-04-25 16:19:57 +0300

    feat: menu food

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ BUTTON_HEIGHT = 60
 MENU_NAV_XPAD = 90
 MENU_NAV_YPAD = 130
 
+FOOD_SIZE = 200
+
 font = pg.font.Font(None, 40)
 font_mini = pg.font.Font(None, 15)
 
@@ -34,6 +36,7 @@ def load_image(file, width, height):
 
 def text_render(text):
     return font.render(str(text), True, "black")
+
 
 
 class Item:
@@ -165,6 +168,119 @@ class ClothesMenu:
         screen.blit(self.use_text, self.use_text_rect)
         screen.blit(self.buy_text, self.buy_text_rect)
 
+class Food:
+    def __init__(self, name, image, price, file, satiety, medicine_power=0):
+        self.name = name
+        self.image = image
+        self.price = price
+        self.satiety = satiety
+        self.medicine_power = medicine_power
+        self.image = load_image(image, FOOD_SIZE, FOOD_SIZE)
+
+
+class MenuFood:
+    def __init__(self, game):
+        self.game = game
+        self.menu_page = load_image("images/menu/menu_page.png", SCREEN_WIDTH, SCREEN_HEIGHT)
+
+        self.bottom_label_off = load_image("images/menu/bottom_label_off.png", SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.bottom_label_on = load_image("images/menu/bottom_label_on.png", SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.top_label_off = load_image("images/menu/top_label_off.png", SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.top_label_on = load_image("images/menu/top_label_on.png", SCREEN_WIDTH, SCREEN_HEIGHT)
+
+        self.items = [
+            Food("Мясо", 30, "images/food/meat.png", 10),
+            Food("Корм", 40, "images/food/dog food.png", 15),
+            Food("Элитный корм", 100, "images/food/dog food elite.png", 25, medicine_power=2),
+            Food("Лекарство", 200, "images/food/medicine.png", 0, medicine_power=10)
+        ]
+
+        self.current_item = 0
+
+        self.item_rect = self.items[0].image.get_rect()
+        self.item_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
+        self.next_button = Button("Вперёд", SCREEN_WIDTH - MENU_NAV_XPAD - BUTTON_WIDTH, SCREEN_HEIGHT - MENU_NAV_YPAD,
+                                  width=int(BUTTON_WIDTH // 1.2), height=int(BUTTON_HEIGHT // 1.2), func=self.to_next)
+
+        self.previous_button = Button("Назад", MENU_NAV_XPAD + 30, SCREEN_HEIGHT - MENU_NAV_YPAD,
+                                      width=int(BUTTON_WIDTH // 1.2), height=int(BUTTON_HEIGHT // 1.2),
+                                      func=self.to_previous)
+
+
+
+        self.buy_button = Button("Съесть", SCREEN_WIDTH // 2 - int(BUTTON_WIDTH // 1.5) // 2,
+                                 SCREEN_HEIGHT // 2 + 95,
+                                 width=int(BUTTON_WIDTH // 1.5), height=int(BUTTON_HEIGHT // 1.5),
+                                 func=self.buy)
+
+        self.price_text = text_render(self.items[self.current_item].price)
+        self.price_text_rect = self.price_text.get_rect()
+        self.price_text_rect.center = (SCREEN_WIDTH // 2, 180)
+
+        self.name_text = text_render(self.items[self.current_item].name)
+        self.name_text_rect = self.name_text.get_rect()
+        self.name_text_rect.center = (SCREEN_WIDTH // 2, 120)
+
+
+
+    def update(self):
+        self.next_button.update()
+        self.previous_button.update()
+        self.buy_button.update()
+
+    def is_clicked(self, event):
+        self.next_button.is_clicked(event)
+        self.previous_button.is_clicked(event)
+        self.buy_button.is_clicked(event)
+
+    def to_next(self):
+        if self.current_item != len(self.items) - 1:
+            self.current_item += 1
+
+        self.price_text = text_render(self.items[self.current_item].price)
+        self.price_text_rect = self.price_text.get_rect()
+        self.price_text_rect.center = (SCREEN_WIDTH // 2, 180)
+
+        self.name_text = text_render(self.items[self.current_item].name)
+        self.name_text_rect = self.name_text.get_rect()
+        self.name_text_rect.center = (SCREEN_WIDTH // 2, 120)
+
+    def to_previous(self):
+        if self.current_item != 0:
+            self.current_item -= 1
+
+        self.price_text = text_render(self.items[self.current_item].price)
+        self.price_text_rect = self.price_text.get_rect()
+        self.price_text_rect.center = (SCREEN_WIDTH // 2, 180)
+
+        self.name_text = text_render(self.items[self.current_item].name)
+        self.name_text_rect = self.name_text.get_rect()
+        self.name_text_rect.center = (SCREEN_WIDTH // 2, 120)
+
+    def buy(self):
+        if self.game.money >= self.items[self.current_item].price:
+            self.game.money -= self.items[self.current_item].price
+            self.game.satiety += self.items[self.current_item].satiety
+            if self.game.satiety > 100:
+                self.game.satiety = 100
+            self.game.health += self.items[self.current_item].medicine_power
+            if self.game.health > 100:
+                self.game.health = 100
+
+
+
+    def draw(self, screen):
+        screen.blit(self.menu_page, (0, 0))
+
+        screen.blit(self.items[self.current_item].image, self.item_rect)
+
+        self.next_button.draw(screen)
+        self.previous_button.draw(screen)
+        self.buy_button.draw(screen)
+
+        screen.blit(self.price_text, self.price_text_rect)
+        screen.blit(self.name_text, self.name_text_rect)
 
 class Button:
     def __init__(self, text, x, y, width=BUTTON_WIDTH, height=BUTTON_HEIGHT, text_font=font, func=None):
