5f66582c37be32dbe1eecae3bc093eb944f5a94a
LOVELY <kotagindeman@gmail.com>
2025-05-05 17:10:36 +0300

    feat: add class dog

--- main.py
+++ main.py
@@ -289,6 +289,18 @@ class Dog(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
 
+        self.image = load_image('images/dog.png', DOG_WIDTH // 2, DOG_HEIGHT // 2)
+        self.rect = self.image.get_rect()
+
+        self.rect.centerx = SCREEN_WIDTH // 2
+        self.rect.centery = SCREEN_HEIGHT - MENU_NAV_YPAD - 10
+
+    def update(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_a]:
+            self.rect.x -= 2
+        if keys[pg.K_d]:
+            self.rect.x += 2
 
 class MiniGame:
     def __init__(self, game):
