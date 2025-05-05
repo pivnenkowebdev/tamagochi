d3dccd55e7ad346859a6fccf4ea7968e4a358898
LOVELY <kotagindeman@gmail.com>
2025-05-05 17:49:40 +0300

    fix: display dog

--- main.py
+++ main.py
@@ -339,12 +339,26 @@ class MiniGame:
         self.interval = 1000 * 5
 
     def update(self):
-        ...
+
+        if random.randint(0, 100) == 0:
+            self.toys.add(Toy())
+
+        self.dog.update()
+        self.toys.update()
+
+
+        hits = pg.sprite.spritecollide(self.dog, self.toys, True, pg.sprite.collide_rect_ratio(0,6))
+
+        self.score += len(hits)
+
+        if pg.time.get_ticks() - self.start_time > self.interval:
+            self.game.happiness += int(self.score // 2)
+            self.game.mode = 'Main'
 
     def draw(self, screen):
         screen.blit(self.background, (0, 0))
 
-        # screen.blit(self.dog_image, self.dog_rect)
+        screen.blit(self.dog.image, self.dog.rect)
 
         screen.blit(text_render(self.score), (MENU_NAV_XPAD + 20, 80))
 
